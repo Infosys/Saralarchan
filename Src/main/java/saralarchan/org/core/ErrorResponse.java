71233f979d6c0bb30b2b53ed98768b5941ea52c8
Manoj Kumar Agrawal <85951028+manojbglr@users.noreply.github.com>
2023-05-16 13:14:18 +0530

    Add files via upload

---
+++ Src/main/java/saralarchan/org/core/ErrorResponse.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2023 Infosys Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package saralarchan.org.core;
+
+public class ErrorResponse {
+	
+	
+	private int errorCode;
+	private String errorMessage;
+	private String errorDescription;
+	
+	/**
+	 * @return the errorCode
+	 */
+	public int getErrorCode() {
+		return errorCode;
+	}
+	/**
+	 * @param errorCode the errorCode to set
+	 */
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	/**
+	 * @return the errorDesc
+	 */
+	public String getErrorDesc() {
+		return errorDescription;
+	}
+	/**
+	 * @param errorDesc the errorDesc to set
+	 */
+	public void setErrorDesc(String errorDesc) {
+		this.errorDescription = errorDesc;
+	}
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	/**
+	 * @param errorMessage the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}	
+}
